ed3fc9c84403aeced06b6961959c4c2716967d98
shehanp12 <shehanp068@gmail.com>
2020-09-24 17:09:07 +0530

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sun= new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        String a = sc.nextLine();
+        String b=sc.nextLine();
+        String c=sc.nextLine();
 
-        int a = sun.nextInt();
+        System.out.println("================================");
 
-        if(a%2!=0){
-            System.out.println("Weird");
-        }
-        else if(a%2==0 && 2<a && a<=5){
-            System.out.println("Not Weird");
 
-        }
-        else if(a%2==0 && 6<a && a<=20){
-            System.out.println("Weird");
-        }
-        else if(a%2==0 && a>20){
-            System.out.println("Not Weird");
-        }
+
+
+
+        System.out.println("================================");
+
+
+
+
+
+
 
 
 
